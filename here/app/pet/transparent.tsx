f194319fba2ab841e1019d73cac663ac4e52c695
hangyeol <chlgksruf3414@gmail.com>
2025-05-14 13:40:44 +0900

    페트

--- here/app/pet/transparent.tsx
+++ here/app/pet/transparent.tsx
@@ -9,7 +9,7 @@ import {
 } from 'react-native';
 import { useRouter } from 'expo-router';
 
-export default function CupGuide() {
+export default function TransparentPetGuide(): React.JSX.Element {
   const router = useRouter();
 
   return (
@@ -30,7 +30,10 @@ export default function CupGuide() {
       </View>
 
       {/* 스크롤 가능한 내용 */}
-      <ScrollView contentContainerStyle={styles.scrollContent} showsVerticalScrollIndicator={false}>
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}
+        showsVerticalScrollIndicator={false}
+      >
         <Image source={require('../../assets/images/guideline/pet1.png')} style={styles.image} />
         <Text style={styles.description}>
           투명 페트병은{"\n"}
@@ -65,78 +68,80 @@ export default function CupGuide() {
   );
 }
 
-
 const styles = StyleSheet.create({
-    container: { flex: 1, backgroundColor: '#fff', paddingTop: 80, alignItems: 'center' },
-    title: {
-        fontSize: 30,
-        fontFamily: 'ChangwonDangamRoundBold',
-        fontWeight: 'bold',
-        marginBottom: 20,
-    },
-    tabContainer: {
-        justifyContent: 'center',
-        paddingHorizontal: 10,
-        marginBottom: 20,
-    },
-    tab: {
-        backgroundColor: '#eee',
-        borderRadius: 16,
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-        marginRight: 10,
-    },
-    tabText_selected: {
-        fontSize: 14,
-        fontFamily: 'ChangwonDangamRound',
-        fontWeight: '600',
-    },
-    tabText: {
-        fontSize: 14,
-        fontFamily: 'ChangwonDangamRound',
-        fontWeight: '600',
-        color: '#9E9E9E',
-    },
-    image: {
-        width: 280,
-        height: 200,
-        resizeMode: 'contain',
-    },
-    description: {
-        fontSize: 16,
-        fontFamily: 'ChangwonDangamRound',
-        textAlign: 'center',
-        paddingHorizontal: 20,
-    },
-    description_last: {
-        fontSize: 16,
-        fontFamily: 'ChangwonDangamRound',
-        textAlign: 'center',
-        paddingHorizontal: 20,
-        marginBottom: 30,
-    },
-    footer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        paddingVertical: 10,
-        paddingBottom: 24,
-        borderTopWidth: 1,
-        borderTopColor: '#ccc',
-        backgroundColor: '#fff',
-        width: '100%',
-    },
-    footerItem: {
-        alignItems: 'center',
-    },
-    icon: {
-        width: 40,
-        height: 40,
-        marginBottom: 4,
-    },
-    footerText: {
-        fontFamily: 'ChangwonDangamRound',
-        fontSize: 12,
-        fontWeight: '500',
-        color: '#000',
-    },
+  container: { flex: 1, backgroundColor: '#fff', paddingTop: 80, alignItems: 'center' },
+  title: {
+    fontSize: 30,
+    fontFamily: 'ChangwonDangamRoundBold',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  tabContainer: {
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  tab: {
+    backgroundColor: '#eee',
+    borderRadius: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    marginRight: 10,
+  },
+  tabText_selected: {
+    fontSize: 14,
+    fontFamily: 'ChangwonDangamRound',
+    fontWeight: '600',
+  },
+  tabText: {
+    fontSize: 14,
+    fontFamily: 'ChangwonDangamRound',
+    fontWeight: '600',
+    color: '#9E9E9E',
+  },
+  scrollContent: {
+    alignItems: 'center',
+  },
+  image: {
+    width: 280,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  description: {
+    fontSize: 16,
+    fontFamily: 'ChangwonDangamRound',
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+  description_last: {
+    fontSize: 16,
+    fontFamily: 'ChangwonDangamRound',
+    textAlign: 'center',
+    paddingHorizontal: 20,
+    marginBottom: 30,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 10,
+    paddingBottom: 24,
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+    backgroundColor: '#fff',
+    width: '100%',
+  },
+  footerItem: {
+    alignItems: 'center',
+  },
+  icon: {
+    width: 40,
+    height: 40,
+    marginBottom: 4,
+  },
+  footerText: {
+    fontFamily: 'ChangwonDangamRound',
+    fontSize: 12,
+    fontWeight: '500',
+    color: '#000',
+  },
 });
